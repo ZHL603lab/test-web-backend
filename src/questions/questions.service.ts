766ce66ef3cb629a0ae76a5679fb62252b1ff0ad
Carmine Carlo <3340751717@qq.com>
2023-11-16 01:25:07 +0800

    :construction: feat(questions): Create RESTful style entry point

---
+++ src/questions/questions.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateQuestionDto } from './dto/create-question.dto';
+import { UpdateQuestionDto } from './dto/update-question.dto';
+import { PrismaService } from "../prisma/prisma.service";
+
+@Injectable()
+export class QuestionsService {
+  constructor(
+    private readonly prismaService: PrismaService
+  ) {
+  }
+  create(createQuestionDto: CreateQuestionDto) {
+    return 'This action adds a new question';
+  }
+
+  findAll() {
+    return this.prismaService.question.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} question`;
+  }
+
+  update(id: number, updateQuestionDto: UpdateQuestionDto) {
+    return `This action updates a #${id} question`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} question`;
+  }
+}
